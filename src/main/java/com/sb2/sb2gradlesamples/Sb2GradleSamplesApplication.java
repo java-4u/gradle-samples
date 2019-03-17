f9f1eb3ac349ef6fbbe8d3e99f074a7e8ca27dba
kmk572 <kmk572@gmail.com>
2019-03-17 23:21:39 +0530

    first commit

---
+++ src/main/java/com/sb2/sb2gradlesamples/Sb2GradleSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.sb2.sb2gradlesamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2GradleSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2GradleSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb2/sb2gradlesamples/Sb2GradleSamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sb2.sb2gradlesamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sb2GradleSamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
